f7290cab87933d9b66cfbacce233b81fda5de75d
plaban <plaban.patra@sc.com>
2020-03-13 10:23:58 +0530

    Cucumber Test and dependency added

---
+++ src/main/java/com/iostestdemo/wrapper/IOSNativePage.java
@@ -0,0 +1,11 @@
+/**
+ */
+package com.iostestdemo.wrapper;
+public class IOSNativePage extends NativePage {
+	/**
+	 * A constructor to initialize PageFactory for all child Native pages
+	 */
+//	public IOSNativePage() {
+//		
+//	}
+}
---
+++ src/main/java/com/iostestdemo/wrapper/NativePage.java
@@ -0,0 +1,10 @@
+package com.iostestdemo.wrapper;
+public class NativePage extends Page {
+	/**
+	 * 
+	 */
+	public NativePage() {
+		GenericWrappers.switchToNativeApp();
+	}
+
+}
